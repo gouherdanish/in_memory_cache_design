3b645de77a9f725f29158b51f7e3e3497625230f
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-08-10 15:11:34 +0530

    Implemented load and save methods for Json Persistance

--- factory/persistance_factory.py
+++ factory/persistance_factory.py
@@ -1,4 +1,26 @@
+import os, json
+
+from constants import PathConstants
+from cache_entry import CacheEntry
+
 class JsonPersistance:
-    def __init__(self) -> None:
-        self.file_path = '/Users/gouher/Documents/personal/codes/system_design/in_memory_cache_design/volume/volume1.json'
-        
+    def __init__(self,file_path) -> None:
+        self.file_path = os.path.join(PathConstants.PERSISTANCE_DIR,'cache.json')
+    
+    def load(self):
+        """
+        Loads the persisted file from disk volume into memory
+        """
+        if os.path.exists(self.file_path):
+            with open(self.file_path,'r') as f:
+                data = json.load(f)
+            return {k:CacheEntry(v['value'],v['expiration']) for k,v in data.items()}
+        return {}
+    
+    def save(self,cache):
+        """
+        Persists the cached data onto disk
+        """
+        data = {k:{'value':v.value,'expiration':v.expiration} for k,v in cache.items()}
+        with open(self.file_path,'w') as f:
+            json.dump(data,f)
