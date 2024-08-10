188f66f44f7f35be4afd18185677bb036de9d770
Gouher Danish <gouherdanishiitkgp@gmail.com>
2024-08-10 15:13:03 +0530

    Added Worker factory to handle different background threads

---
+++ factory/worker_factory.py
@@ -0,0 +1,46 @@
+from abc import ABC, abstractmethod
+import threading
+import time
+
+class WorkerFactory:
+    registry = {}
+
+    @classmethod
+    def register_manager(cls,how):
+        def wrap_fn(wrapped_cls):
+            cls.registry[how] = wrapped_cls
+            return wrapped_cls
+        return wrap_fn
+    
+    @classmethod
+    def get_manager(cls,how,**kwargs):
+        print(how,kwargs)
+        try:
+            return cls.registry[how](**kwargs)
+        except:
+            raise ValueError(f'Invalid Worker Requested: {how}')
+
+class Worker(ABC):
+    def __init__(self) -> None:
+        self._stop_event = threading.Event()
+
+    def start(self,target):
+        self._thread = threading.Thread(target=target, daemon=True)
+        self._thread.start()
+
+    def stop(self):
+        self._stop_event.set()
+        self._thread.join()
+
+    def pause(self,interval):
+        print(f'Thread going to sleep for {interval} s')
+        time.sleep(interval)
+
+@WorkerFactory.register_worker('cleanup')
+class CleanupWorker(Worker):
+    pass
+
+@WorkerFactory.register_worker('persistance')
+class PersistanceWorker(Worker):
+    pass
+
